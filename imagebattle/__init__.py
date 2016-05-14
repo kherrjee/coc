8d2769c4cb6d3e20a2ad735e5978d8fb5670199d
unknown <training.python@pnp-d13.pnp.local>
2016-05-14 14:30:21 +0800

    Run database

--- imagebattle/__init__.py
+++ imagebattle/__init__.py
@@ -6,8 +6,6 @@ app.config.from_object('imagebattle.config')
 from . import hooks  # noqa
 from .import views  # noqa
 
-<<<<<<< HEAD
+
 db = SQLAlchemy(app)
-=======
-db = SQLAlchemy(app)
->>>>>>> 7e16aa5c71b24291e38f062f37d728ad4a14b646
+
--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from imagebattle import app
 from imagebattle import db
-<<<<<<< HEAD
-=======
-
->>>>>>> 7e16aa5c71b24291e38f062f37d728ad4a14b646
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-<<<<<<< HEAD
 db.create_all()
-=======
-db.create_all()
->>>>>>> 7e16aa5c71b24291e38f062f37d728ad4a14b646
+
+
